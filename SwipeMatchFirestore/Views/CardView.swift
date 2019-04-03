ee4ed61845a95cd97c46cc7367201ab92480411b
Ryan Hutchison <rwhutchison@live.com>
2019-04-02 23:13:08 -0500

    Dismiss cards to right or left

--- SwipeMatchFirestore/Views/CardView.swift
+++ SwipeMatchFirestore/Views/CardView.swift
@@ -12,6 +12,9 @@ class CardView: UIView {
     
     fileprivate let imageView = UIImageView(image: UIImage(named: "lady5c"))
     
+    // Configurations
+    fileprivate let threshold: CGFloat = 80
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         layer.cornerRadius = 10
@@ -24,16 +27,12 @@ class CardView: UIView {
         addGestureRecognizer(panGesture)
     }
     
-    
-    
-    
-    
     @objc fileprivate func handlePan(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .changed:
             handleChanged(gesture)
         case .ended:
-            handleEnded()
+            handleEnded(gesture)
         default:
             ()
         }
@@ -41,14 +40,25 @@ class CardView: UIView {
     
     fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: nil)
-        self.transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+        let degrees: CGFloat = translation.x / 20
+        let angle = degrees * .pi / 180
+        let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+        self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
     }
     
-    fileprivate func handleEnded() {
+    fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+        let translationDirection: CGFloat = gesture.translation(in: nil).x > 0 ? 1 : -1
+        let shouldDismissCard = abs(gesture.translation(in: nil).x) > threshold
+        
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-            self.transform = .identity
+            if shouldDismissCard {
+                self.frame = CGRect(x: 1000 * translationDirection, y: 0, width: self.frame.width, height: self.frame.height)
+            } else {
+                self.transform = .identity
+            }
         }) { (_) in
-            
+            self.transform = .identity
+            self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
         }
     }
     
